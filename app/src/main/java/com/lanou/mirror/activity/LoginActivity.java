489c8a3a31b4b2ad23d69e5f6ad2c46a630f0e8e
何伟东 <970533361@qq.com>
2016-03-30 09:03:11 +0800

    注册登录合并更改代码

--- app/src/main/java/com/lanou/mirror/activity/LoginActivity.java
+++ app/src/main/java/com/lanou/mirror/activity/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.lanou.mirror.activity;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 import com.lanou.mirror.R;
 import com.lanou.mirror.base.BaseActivity;
 
 /**
  * Created by Yi on 16/3/29.
  */
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+    private Button createBtn;
     @Override
     protected int setContent() {
         return R.layout.activity_login;
@@ -14,11 +19,20 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        createBtn = bindView(R.id.activity_login_create_btn);
 
     }
 
     @Override
     protected void initData() {
+        createBtn.setOnClickListener(this);
 
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(intent);
     }
 }
