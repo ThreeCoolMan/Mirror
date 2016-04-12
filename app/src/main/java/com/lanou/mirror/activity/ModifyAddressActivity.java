1ce4d29c849fe75dcc17985c59a0e595170bb709
何伟东 <970533361@qq.com>
2016-04-12 08:53:27 +0800

    no message

---
+++ app/src/main/java/com/lanou/mirror/activity/ModifyAddressActivity.java
@@ -0,0 +1,26 @@
+package com.lanou.mirror.activity;
+
+import android.text.Layout;
+
+import com.lanou.mirror.R;
+import com.lanou.mirror.base.BaseActivity;
+
+/**
+ * Created by dllo on 16/4/11.
+ */
+public class ModifyAddressActivity extends BaseActivity {
+    @Override
+    protected int setContent() {
+        return R.layout.activity_modifyaddress;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
--- app/src/main/java/com/lanou/mirror/activity/MyAllAddressActivity.java
+++ app/src/main/java/com/lanou/mirror/activity/MyAllAddressActivity.java
@@ -1,6 +1,9 @@
 package com.lanou.mirror.activity;
 
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.lanou.mirror.R;
@@ -26,6 +29,7 @@ public class MyAllAddressActivity extends BaseActivity implements UrlListener, O
     private MyAllAddressAdapter adapter;
 
     private String token = "";
+    private String addrId = "182";
 
 
     @Override
@@ -42,6 +46,8 @@ public class MyAllAddressActivity extends BaseActivity implements UrlListener, O
 
     @Override
     protected void initData() {
+
+
         token = getIntent().getStringExtra("token");
         HashMap<String, String> params = new HashMap<>();
         params.put("token", token);
@@ -49,6 +55,7 @@ public class MyAllAddressActivity extends BaseActivity implements UrlListener, O
         params.put("page", "");
 
         OkHttpNetHelper.getOkHttpNetHelper().postRequest(USER_ADDRESS_LIST_URL, params, MyAllAddressBeans.class, this);
+
     }
 
     @Override
@@ -58,7 +65,33 @@ public class MyAllAddressActivity extends BaseActivity implements UrlListener, O
             @Override
             public void run() {
                 adapter = new MyAllAddressAdapter((ArrayList<MyAllAddressBeans.DataEntity.ListEntity>) bean.getData().getList(), MyAllAddressActivity.this);
+
                 listView.setAdapter(adapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        Intent intent = new Intent(MyAllAddressActivity.this,BuyDetailsActivity.class);
+                        HashMap<String,String> map = new HashMap<String, String>();
+                        //去服务器提交新的默认地址让其更改
+                        map.put("token",token);
+                        map.put("addr_id",addrId);
+                        intent.putExtra("token",token);
+                        OkHttpNetHelper.getOkHttpNetHelper().postStringRequest(UESR_DEFAULT_ADRESS_URL, map, new OkHttpNetHelperListener() {
+                            @Override
+                            public void requestSucceed(String result, Object bean) {
+
+                            }
+
+                            @Override
+                            public void requestFailed(String cause) {
+
+                            }
+                        });
+                        startActivity(intent);
+
+                    }
+                });
             }
         });
 
@@ -68,4 +101,5 @@ public class MyAllAddressActivity extends BaseActivity implements UrlListener, O
     public void requestFailed(String cause) {
 
     }
+
 }
