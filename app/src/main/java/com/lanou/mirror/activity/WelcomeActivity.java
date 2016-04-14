e23a87bc1c3753c8c74a74fcf73d434a519d5d9e
张洪猛 <1487165975@qq.com>
2016-04-14 08:40:56 +0800

    欢迎页加了轻量级缓存

--- app/src/main/java/com/lanou/mirror/activity/WelcomeActivity.java
+++ app/src/main/java/com/lanou/mirror/activity/WelcomeActivity.java
@@ -1,15 +1,15 @@
 package com.lanou.mirror.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Handler;
 import android.util.Log;
-import android.widget.ImageView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.lanou.mirror.R;
 import com.lanou.mirror.base.BaseActivity;
-import com.lanou.mirror.bean.GoodsListBeans;
 import com.lanou.mirror.bean.WelcomeBean;
 import com.lanou.mirror.listener.OkHttpNetHelperListener;
 import com.lanou.mirror.listener.UrlListener;
@@ -25,6 +25,7 @@ public class WelcomeActivity extends BaseActivity implements UrlListener, OkHttp
     private Handler handler = new Handler();
     private final long SPLASH_LENGTH = 3000;
 
+    private SharedPreferences preferences;
 
     @Override
     protected int setContent() {
@@ -38,17 +39,36 @@ public class WelcomeActivity extends BaseActivity implements UrlListener, OkHttp
 
     @Override
     protected void initData() {
-        HashMap<String, String> params = new HashMap<>();
-        params.put("version", "1.0.1");
-        OkHttpNetHelper.getOkHttpNetHelper().postRequest(START_IMAGE_URL, params, WelcomeBean.class, this);
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, SPLASH_LENGTH);
+        SharedPreferences preferences = getSharedPreferences("imgUrl", Context.MODE_PRIVATE);
+        String url = preferences.getString("url","");
+        Log.d("走没",url+"url");
+        if (!url.equals("")){
+            Log.d("走没","用到缓存");
+            welcomeIv.setImageURI(Uri.parse(url));
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, SPLASH_LENGTH);
+        }else {
+
+            HashMap<String, String> params = new HashMap<>();
+            params.put("version", "1.0.1");
+            OkHttpNetHelper.getOkHttpNetHelper().postRequest(START_IMAGE_URL, params, WelcomeBean.class, this);
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, SPLASH_LENGTH);
+        }
+
+
 
 
     }
@@ -59,12 +79,16 @@ public class WelcomeActivity extends BaseActivity implements UrlListener, OkHttp
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
+                Log.d("走没","第一次进来");
                 String url = bean.getImg();
-
+                SharedPreferences preferences = getSharedPreferences("imgUrl", MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("url",url);
+                editor.commit();
                 welcomeIv.setImageURI(Uri.parse(url));
 
 
-//                OkHttpNetHelper.getOkHttpNetHelper().setOkImage("http://pic1.zhimg.com/e1cc747cbf2076a378d2fe0f8c3b2e20.jpg", welcomeIv);
+
 
             }
         });
